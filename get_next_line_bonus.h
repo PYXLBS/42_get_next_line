818e770840152a38fc532cd5c79b17549b1a6e3a
pyxlbs <polaberto@protonmail.com>
2023-10-14 15:01:42 +0200

    *

---
+++ get_next_line_bonus.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabertha <pabertha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/20 13:21:45 by pabertha          #+#    #+#             */
+/*   Updated: 2023/10/13 16:14:35 by pabertha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+# include <stdlib.h>
+# include <unistd.h>
+# include <ctype.h>
+# include <string.h>
+# include <strings.h>
+# include <stdio.h>
+# include <stdint.h>
+# include <limits.h>
+# include <stdarg.h>
+# include <fcntl.h>
+
+char		*get_next_line(int fd);
+char		*ft_strchr(const char *s, int c);
+size_t		ft_strlen(const char *s);
+size_t		ft_strlcat(char *dst, const char *src, size_t size);
+void		*ft_calloc(size_t nmemb, size_t size);
+char		*ft_strjoin(char *s1, char *s2);
+char		*free_reset_return(char *to_reset, char *reset_to, char *to_return);
+void		update_all(char *buffer, char *line);
+
+#endif
